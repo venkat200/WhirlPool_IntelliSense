fe68913d23297a3a6c2e61d00d20e4d01fd0abea
VENKAT <34069910+venkat200@users.noreply.github.com>
2020-10-01 21:31:58 +0530

    fix

--- Assets/Zappar/Scripts/ZapparInstantTrackingTarget.cs
+++ Assets/Zappar/Scripts/ZapparInstantTrackingTarget.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Collections;
 
 using Zappar;
+using UnityEngine.EventSystems;
 
 public class ZapparInstantTrackingTarget : ZapparTrackingTarget, ZapparCamera.ICameraListener
 {
@@ -12,9 +13,39 @@ public class ZapparInstantTrackingTarget : ZapparTrackingTarget, ZapparCamera.IC
     private bool m_hasInitialised = false;
     private bool m_isMirrored = false;
 
+    public GameObject BackGroundCanvas;
+    public GameObject LoadingBar;
+    public GameObject SceneObject;
+ 
+    bool pinClick = false;
+
+    // Touch Input Check
+    private Touch theTouch;
+    private float timeTouchBegan;
+    private float timeTouchEnded;
+    private Vector2 startPosition, endPosition;
+    private float displayTime = 0.5f;
+    private Touch touch;
+    bool pinTry = false;
+    // Touch Input Check
+
     void Start()
     {
         ZapparCamera.Instance.RegisterCameraListener( this );
+    }
+
+    void OnEnable()
+    {
+        BackGroundCanvas.SetActive(false);
+        LoadingBar.SetActive(true);
+        SceneObject.transform.localScale = new Vector3(3f, 3f, 3f);
+    }
+
+    void OnDisable()
+    {
+        BackGroundCanvas.SetActive(true);
+        LoadingBar.SetActive(false);
+        SceneObject.transform.localScale = new Vector3(1f, 1f, 1f);
     }
 
     public void OnZapparInitialised(IntPtr pipeline) 
@@ -55,9 +86,39 @@ public class ZapparInstantTrackingTarget : ZapparTrackingTarget, ZapparCamera.IC
         if (Input.touchCount > 0)
         {
             m_userHasPlaced = !m_userHasPlaced;
+            UpdateTargetPose();
         }
 
-        UpdateTargetPose();
+        // UpdateTargetPose();
+
+        if (Input.touchCount > 0)
+        {
+            theTouch = Input.GetTouch(0);
+            // Check if finger is over a UI element
+            if (!EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId))
+            {
+                if (theTouch.phase == TouchPhase.Began)
+                {
+                    startPosition = theTouch.position;
+                    timeTouchBegan = Time.time;
+                }
+                if (theTouch.phase == TouchPhase.Ended)
+                {
+                    pinTry = true;
+                    endPosition = theTouch.position;
+                    timeTouchEnded = Time.time;
+                }
+            }
+        }
+        // else if ((Time.time - timeTouchEnded > displayTime) && pinTry==true && (timeTouchEnded - timeTouchBegan < 1f))
+        else if (pinTry == true && (timeTouchEnded - timeTouchBegan < 1f) && (Vector2.Distance(startPosition, endPosition) < 4f))
+        {
+            pinClick = !pinClick;
+            m_userHasPlaced = !m_userHasPlaced;
+            LoadingBar.SetActive(!pinClick);
+            pinTry = false;
+        }
+
     }
 
     void OnDestroy()
